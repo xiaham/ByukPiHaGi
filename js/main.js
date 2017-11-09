24548e068a9143de41e5eca588364ea4de49888c
Haemin <31715504+xiaham@users.noreply.github.com>
2017-11-09 16:45:32 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var bird = {
 	width: 40,
 	height: 40,
 	yVelocity : 0,
-	yAcceleration: 2,
+	yAcceleration: 1,
 	draw : function(){
 		image(image_bird, this.x, this.y, this.width, this.height);
 	},
